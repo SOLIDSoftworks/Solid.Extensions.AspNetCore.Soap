b0ce8c3216cff5ff0381e11ff24e98f264eeec80
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-06-18 20:26:48 +0000

    Update ISoapApplicationBuilder.cs

--- src/Solid.Extensions.AspNetCore.Soap/Builder/ISoapApplicationBuilder.cs
+++ src/Solid.Extensions.AspNetCore.Soap/Builder/ISoapApplicationBuilder.cs
@@ -8,8 +8,8 @@ namespace Solid.Extensions.AspNetCore.Soap.Builder
     /// <summary>
     /// An interface that enables extending a SOAP endpoint.
     /// <para>
-    ///   This is nothing more that an <see cref="IApplicationBuilder" />. 
-    ///   It's a seperate interface so that specific SOAP middleware extension methods can be created.
+    /// This is nothing more that an <see cref="IApplicationBuilder" />. 
+    /// It's a seperate interface so that specific SOAP middleware extension methods can be created.
     /// </para>
     /// </summary>
     public interface ISoapApplicationBuilder : IApplicationBuilder
