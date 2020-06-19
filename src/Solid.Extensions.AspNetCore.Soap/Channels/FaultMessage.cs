c2491e9f03d2723e8f9adfb68bb44f9a0914a601
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-06-19 10:36:33 +0000

    fixes error in Soap 1.1 fault response

--- src/Solid.Extensions.AspNetCore.Soap/Channels/FaultMessage.cs
+++ src/Solid.Extensions.AspNetCore.Soap/Channels/FaultMessage.cs
@@ -89,7 +89,7 @@ namespace Solid.Extensions.AspNetCore.Soap.Channels
                     using (var reader = Fault.GetReaderAtDetailContents())
                     {
                         writer.WriteStartElement("detail");
-                        writer.WriteRaw(reader.ReadOuterXml());
+                        writer.WriteElement(reader);
                         writer.WriteEndElement();
                     }
                 }
@@ -146,34 +146,9 @@ namespace Solid.Extensions.AspNetCore.Soap.Channels
                 if (Fault.HasDetail)
                 {
                     using (var reader = Fault.GetReaderAtDetailContents())
-                    using(var inner = reader.ReadSubtree())
                     {
                         writer.WriteStartElement("Detail", SoapConstants.Soap12EnvelopeNamespace);
-                        while(inner.Read())
-                        {
-                            if (inner.NodeType == XmlNodeType.Element)
-                            {
-                                if (string.IsNullOrEmpty(inner.NamespaceURI))
-                                    writer.WriteStartElement(inner.LocalName);
-                                else
-                                    writer.WriteStartElement(inner.LocalName, inner.NamespaceURI);
-
-                                writer.WriteAttributes(inner, false);
-                                continue;
-                            }
-
-                            if (inner.NodeType == XmlNodeType.Text)
-                            {
-                                writer.WriteValue(inner.Value);
-                                continue;
-                            }
-
-                            if (inner.NodeType == XmlNodeType.EndElement)
-                            {
-                                writer.WriteEndElement();
-                                continue;
-                            }
-                        }
+                        writer.WriteElement(reader);
                         writer.WriteEndElement();
                     }
                 }
