99c6be8b106f9f703a2e72d6e6e7a02d3c071749
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-06-19 14:49:03 +0000

    Update SoapTestingServerFixture.cs

--- src/Solid.Testing.AspNetCore.Extensions.XUnit.Soap/SoapTestingServerFixture.cs
+++ src/Solid.Testing.AspNetCore.Extensions.XUnit.Soap/SoapTestingServerFixture.cs
@@ -28,7 +28,7 @@ namespace Solid.Testing.AspNetCore.Extensions.XUnit.Soap
             };
             var encoding = binding.Elements.Find<TextMessageEncodingBindingElement>();
             encoding.MessageVersion = context.MessageVersion;
-            return binding.WithSolidHttpTransport(TestingServer);
+            return binding;
         }
 
         protected virtual EndpointAddress CreateEndpointAddress<TChannel>(Uri url, SoapChannelCreationContext context)
@@ -49,7 +49,7 @@ namespace Solid.Testing.AspNetCore.Extensions.XUnit.Soap
                 if (context.Path != null)
                     url = new Uri(url, context.Path);
                 
-                var binding = CreateBinding<TChannel>(k, context);
+                var binding = CreateBinding<TChannel>(k, context).WithSolidHttpTransport(TestingServer);
                 var endpointAddress = CreateEndpointAddress<TChannel>(url, context);
                 var factory = CreateChannelFactory<TChannel>(binding, endpointAddress, context);
                 var channel = CreateChannel(factory, context);
